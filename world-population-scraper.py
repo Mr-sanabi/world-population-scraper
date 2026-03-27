dad6b3f5c8928c761cd4130d7e872a33d34ba259
Mr-sanabi <mentx.yt@gmail.com>
2026-03-27 19:49:46 +0100

    Update world-population-scraper.py

--- world-population-scraper.py
+++ world-population-scraper.py
@@ -1,9 +1,9 @@
 import csv, requests
 from bs4 import BeautifulSoup
 
-FILENAME = "world-population.csv"
+FILENAME = "world-population111.csv"
 
-data = [["name","population","yearly_change","net_change","density","land_area","median_age","urban_pop"]]
+data = [["Name","Population","Yearly_Change","Net_Change","Density","Land_Area","Median_Age","Urban_Pop"]]
 url = f'https://www.worldometers.info/world-population/population-by-country/'
 response = requests.get(url)
 response.encoding = "utf-8"
@@ -22,15 +22,12 @@ for row in rows:
         Net_Change = cols[4].text.strip()
         Density = cols[5].text.strip()
         Land_Area = cols[6].text.strip()
-        Median_Age = cols[7].text.strip()
-        Urban_Pop = cols[8].text.strip()
+        Median_Age = cols[9].text.strip()
+        Urban_Pop = cols[10].text.strip()
         urban = Urban_Pop + "%"
 
         data.append([name,population,yr_change,Net_Change,Density,Land_Area,Median_Age,urban])
-    
-          
-    
-    
+        
 with open(FILENAME, "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerows(data)
